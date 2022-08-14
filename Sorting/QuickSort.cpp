0c60e19ab7a4ff7e2b49b1ef9aa78d3eb0e315b5
Sachin <89219332+Sachin737@users.noreply.github.com>
2022-08-14 21:42:17 +0530

    Add files via upload

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Time complexity : 
+//     randomized pivot : worst case O(n*n)
+//                      : average case O(n*logn)   
+
+
+int partition(vector<int>&v,int st,int en)
+{
+    int pivot = v[en];
+    int pindex = st;
+
+    for(int i=st;i<en;i++)
+    {
+        if(v[i]<=pivot)
+        {
+            swap(v[i],v[pindex]);
+            pindex++;
+        }
+    }
+    swap(v[pindex],v[en]);
+    return pindex;
+}
+
+void Quicksort(vector<int>&v,int i,int j)
+{
+    if(i<j)
+    {
+        int pivot = partition(v,i,j);
+        Quicksort(v,i,pivot-1);
+        Quicksort(v,pivot+1,j);
+    }
+}
+
+int main()
+{
+    int n;cin >> n;
+    vector<int>v(n);
+    for(auto &x:v) cin >> x;
+
+    Quicksort(v,0,n-1);    
+
+    for(auto &x:v)cout<<x<<" ";
+
+    return 0;
+}
